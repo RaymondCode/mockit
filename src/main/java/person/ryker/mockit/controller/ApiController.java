35e99bc9fd740cdea1d381eb2b23abb55dc4cb2d
RaymondCode <raymondcode@outlook.com>
2017-11-24 10:30:24 +0800

    #12 fix domain

--- src/main/java/person/ryker/mockit/controller/ApiController.java
+++ src/main/java/person/ryker/mockit/controller/ApiController.java
@@ -1,5 +1,10 @@
 package person.ryker.mockit.controller;
 
+import java.io.IOException;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +13,6 @@ import org.springframework.web.multipart.MultipartFile;
 import person.ryker.mockit.model.ApiInfo;
 import person.ryker.mockit.model.CreateApiRequest;
 import person.ryker.mockit.service.ApiService;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * @author raymondcode raymondcode@outlook.com
@@ -27,7 +27,7 @@ public class ApiController {
 
     @RequestMapping("/call/{key}")
     public void call(HttpServletResponse response, @PathVariable String key)
-            throws IOException {
+        throws IOException {
         ApiInfo apiInfo = apiService.callApi(key);
 
         response.setStatus(apiInfo.getStatus());
@@ -37,25 +37,22 @@ public class ApiController {
 
     @RequestMapping("/putFile")
     @ResponseBody
-    public Object putFile(HttpServletRequest request, Integer status, String contentType, MultipartFile file)
-            throws IOException {
+    public Object putFile(Integer status, String contentType, MultipartFile file) throws IOException {
         ApiInfo apiInfo = new ApiInfo();
 
         apiInfo.setStatus(status);
         apiInfo.setContentType(contentType);
         apiInfo.setBody(file.getBytes());
 
-        String rootPath = request.getRequestURL().toString().replace(request.getRequestURI(), request.getContextPath());
         String key = apiService.addApi(apiInfo);
-        return rootPath + "/api/call/" + key;
+        return "https://mockit.info/api/call/" + key;
     }
 
     @RequestMapping("/put")
     @ResponseBody
-    public Object put(HttpServletRequest request, CreateApiRequest apiRequest) throws IOException {
-        String rootPath = request.getRequestURL().toString().replace(request.getRequestURI(), request.getContextPath());
+    public Object put(CreateApiRequest apiRequest) throws IOException {
         String key = apiService.addApi(ApiInfo.fromCreateRequest(apiRequest));
 
-        return rootPath + "/api/call/" + key;
+        return "https://mockit.info/api/call/" + key;
     }
 }
