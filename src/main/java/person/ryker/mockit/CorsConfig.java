aa4686995ae2d5e1dd3f982cd4f886b10c7166f7
RaymondCode <raymondcode@outlook.com>
2017-12-01 14:06:31 +0800

    #23 add CROS and jsonp support

---
+++ src/main/java/person/ryker/mockit/CorsConfig.java
@@ -0,0 +1,29 @@
+package person.ryker.mockit;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author raymondcode raymondcode@outlook.com
+ * @date 2017/12/1
+ */
+@Configuration
+public class CorsConfig {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
--- src/main/java/person/ryker/mockit/controller/ApiController.java
+++ src/main/java/person/ryker/mockit/controller/ApiController.java
@@ -5,9 +5,11 @@ import java.io.IOException;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import person.ryker.mockit.model.ApiInfo;
@@ -25,14 +27,26 @@ public class ApiController {
     @Resource
     private ApiService apiService;
 
+    private static final String JS_TYPE = "application/javascript";
+
+    @ResponseBody
     @RequestMapping("/call/{key}")
-    public void call(HttpServletResponse response, @PathVariable String key)
-        throws IOException {
+    public void call(HttpServletResponse response, @PathVariable String key,
+                     @RequestParam(name = "callback", required = false) String callback) throws IOException {
         ApiInfo apiInfo = apiService.callApi(key);
 
         response.setStatus(apiInfo.getStatus());
-        response.setContentType(apiInfo.getContentType());
-        response.getOutputStream().write(apiInfo.getBody());
+
+        if (StringUtils.isEmpty(callback)) {
+            response.setContentType(apiInfo.getContentType());
+            response.getOutputStream().write(apiInfo.getBody());
+        } else {
+            String content = new String(apiInfo.getBody());
+            String result = String.format(callback + "(%s)", content);
+
+            response.setContentType(JS_TYPE);
+            response.getOutputStream().write(result.getBytes());
+        }
     }
 
     @RequestMapping("/putFile")
